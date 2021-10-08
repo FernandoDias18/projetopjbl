fd81da57188aa8301f987b12e71b9fa18e384b06
Fernando Dias <90704932+FernandoDias18@users.noreply.github.com>
2021-10-08 08:58:01 -0300

    Validando o checkbox Termo de Uso

--- formularios/script-validação/scriptformulario.js
+++ formularios/script-validação/scriptformulario.js
@@ -15,27 +15,31 @@ function validar(){
     var email= document.getElementById("email").value.trim();
     var senha = document.getElementById("senha").value.trim();
     var confirmarsenha = document.getElementById("confirmarsenha").value.trim();
+    var termouso = document.getElementById("termouso");
     if (nome.length < 3 || nome == ""){
-        document.getElementById('enome').innerHTML = `<p>ERRO! Nome inválido</p>`
+        document.getElementById('erronome').innerHTML = `<p>ERRO! Nome inválido</p>`
         document.getElementById("nome").style.border = "solid 1px red"
     }
     if (sobrenome == ""){
-        document.getElementById('esobrenome').innerHTML = `<p>ERRO! Sobrenome inválido</p>`
+        document.getElementById('errosobrenome').innerHTML = `<p>ERRO! Sobrenome inválido</p>`
         document.getElementById("sobrenome").style.border = "solid 1px red"
     }
     if (email == ""){
-        document.getElementById('eemail').innerHTML = `<p>ERRO! E-mail inválido</p>`
+        document.getElementById('erroemail').innerHTML = `<p>ERRO! E-mail inválido</p>`
         document.getElementById("email").style.border = "solid 1px red"
     }
     if (senha == "" || senha.length < 5){
-        document.getElementById('esenha').innerHTML = `<p>ERRO! Senha inválida</p>`
+        document.getElementById('errosenha').innerHTML = `<p>ERRO! Senha inválida</p>`
         document.getElementById("senha").style.border = "solid 1px red"
     }
     if (confirmarsenha != senha){
-        document.getElementById('econfirmarsenha').innerHTML = `<p>ERRO! Senhas diferentes</p>`
+        document.getElementById('erroconfirmarsenha').innerHTML = `<p>ERRO! Senhas diferentes</p>`
         document.getElementById("confirmarsennha").style.border = "solid 1px red"
     }
-    document.getElementById("tela-cadastro").style.height = "870px"
+    if (termouso.checked == false){
+        document.getElementById("errotermouso").innerHTML = `<p>Aceite os termos de uso para continuar</p>`
+    }
+    document.getElementById("tela-cadastro").style.height = "900px"
 }
 function validarempresa() {
     var nome = document.getElementById("nome").value.trim();
@@ -45,29 +49,29 @@ function validarempresa() {
     var confirmarsenha = document.getElementById("confirmarsenha").value.trim();
     var termouso = document.getElementById("termouso");
     if (nome == ""){
-        document.getElementById('enome').innerHTML = `<p>ERRO! Nome da empresa inválido</p>`
+        document.getElementById('erronome').innerHTML = `<p>ERRO! Nome da empresa inválido</p>`
         document.getElementById("nome").style.border = "solid 1px red"
     }
     if (cnpj == "" || cnpj.length < 14 || cnpj.length > 14){
-        document.getElementById('ecnpj').innerHTML = `<p>ERRO! CNPJ inválido</p>`
+        document.getElementById('errocnpj').innerHTML = `<p>ERRO! CNPJ inválido</p>`
         document.getElementById("cnpj").style.border = "solid 1px red"
     }
     if (email == ""){
-        document.getElementById('eemail').innerHTML = `<p>ERRO! E-mail inválido</p>`
+        document.getElementById('erroemail').innerHTML = `<p>ERRO! E-mail inválido</p>`
         document.getElementById("email").style.border = "solid 1px red"
     }
     if (senha == "" || senha.length < 5){
-        document.getElementById('esenha').innerHTML = `<p>ERRO! Senha inválida</p>`
+        document.getElementById('errosenha').innerHTML = `<p>ERRO! Senha inválida</p>`
         document.getElementById("senha").style.border = "solid 1px red"
     }
     if (confirmarsenha != senha){
-        document.getElementById('econfirmarsenha').innerHTML = `<p>ERRO! Senhas diferentes</p>`
+        document.getElementById('erroconfirmarsenha').innerHTML = `<p>ERRO! Senhas diferentes</p>`
         document.getElementById("confirmarsennha").style.border = "solid 1px red"
     }
-    if (termouso.checked) {
-        document.getElementById('etermouso').innerHTML = `<p>ERRO! Senhas diferentes</p>`
-    }
-    document.getElementById("tela-empresa").style.height = "890px"
+    if (termouso.checked == false){
+        document.getElementById("errotermouso").innerHTML = `<p>Aceite os termos de uso para continuar</p>`
+    }    
+    document.getElementById("tela-empresa").style.height = "900px"
     
 
 }
@@ -75,11 +79,11 @@ function validarlogin() {
     var email = document.getElementById("email").value.trim();
     var senha = document.getElementById("senha").value.trim();
     if (email == ""){
-        document.getElementById('eemail').innerHTML = `<p>ERRO! E-mail inválido</p>`
+        document.getElementById('erroemail').innerHTML = `<p>ERRO! E-mail inválido</p>`
         document.getElementById("email").style.border = "solid 1px red"
     }
     if (senha == ""){
-        document.getElementById('esenha').innerHTML = `<p>ERRO! Senha inválida</p>`
+        document.getElementById('errosenha').innerHTML = `<p>ERRO! Senha inválida</p>`
         document.getElementById("senha").style.border = "solid 1px red"
     }
     document.getElementById("tela-login").style.height = "580px"
