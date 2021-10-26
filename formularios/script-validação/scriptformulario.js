c62a9d694a0350e45d37c91f970196d6f69292a5
Fernando Dias <90704932+FernandoDias18@users.noreply.github.com>
2021-10-26 13:54:25 -0300

    Adicionando icones nos formulários

--- formularios/script-validação/scriptformulario.js
+++ formularios/script-validação/scriptformulario.js
@@ -16,30 +16,40 @@ function validaraluno(){
     var senha = document.getElementById("senha").value.trim();
     var confirmarsenha = document.getElementById("confirmarsenha").value.trim();
     var termouso = document.getElementById("termouso");
-    document.getElementById("tela-cadastro").style.height = "940px"
+    document.getElementById("tela-cadastro").style.height = "1020px"
     
-    if (nome.length < 3 || nome == ""){
+    if (nome.length < 3){
         document.getElementById('erronome').innerHTML = `<p>ERRO! Nome inválido</p>`
         document.getElementById("nome").style.border = "solid 1px red"
+        document.getElementById("iconerronome").style.display = "block"
     }
     if (sobrenome == ""){
         document.getElementById('errosobrenome').innerHTML = `<p>ERRO! Sobrenome inválido</p>`
         document.getElementById("sobrenome").style.border = "solid 1px red"
+        document.getElementById("iconerrosobren").style.display = "block"
     }
     if (email == ""){
         document.getElementById('erroemail').innerHTML = `<p>ERRO! E-mail inválido</p>`
         document.getElementById("email").style.border = "solid 1px red"
+        document.getElementById("iconerroemail").style.display = "block"
     }
-    if (senha == "" || senha.length < 5){
+    if (senha.length < 5){
         document.getElementById('errosenha').innerHTML = `<p>ERRO! Senha inválida</p>`
         document.getElementById("senha").style.border = "solid 1px red"
+        document.getElementById("iconerrosenha").style.display = "block"
     }
     if (termouso.checked == false){
         document.getElementById("errotermouso").innerHTML = `<p>Aceite os termos de uso para continuar</p>`
     }
-    if (confirmarsenha != senha){
+   if (confirmarsenha != senha){
         document.getElementById('erroconfirmarsenha').innerHTML = `<p>ERRO! Senhas diferentes</p>`
         document.getElementById("confirmarsennha").style.border = "solid 1px red"
+        document.getElementById("iconerroconfsenha").style.display = "block"
+    }
+    if(nome.length > 3 && sobrenome != "" && email != "" && senha.length > 5 &&
+    confirmarsenha == senha && termouso.checked == true) {
+        alert("CADASTRO REALIZADO COM SUCESSO")
+        window.location.href = "../paginas/login.html";
     }
 }
 function validarempresa() {
@@ -49,23 +59,27 @@ function validarempresa() {
     var senha = document.getElementById("senha").value.trim();
     var confirmarsenha = document.getElementById("confirmarsenha").value.trim();
     var termouso = document.getElementById("termouso");
-    document.getElementById("tela-empresa").style.height = "940px"
+    document.getElementById("tela-empresa").style.height = "1150px"
     
     if (nome == ""){
         document.getElementById('erronome').innerHTML = `<p>ERRO! Nome da empresa inválido</p>`
         document.getElementById("nome").style.border = "solid 1px red"
+        document.getElementById("iconerronome").style.display = "block"
     }
     if (cnpj == "" || cnpj.length < 14 || cnpj.length > 14){
         document.getElementById('errocnpj').innerHTML = `<p>ERRO! CNPJ inválido</p>`
         document.getElementById("cnpj").style.border = "solid 1px red"
+        document.getElementById("iconerrocnpj").style.display = "block"
     }
     if (email == ""){
         document.getElementById('erroemail').innerHTML = `<p>ERRO! E-mail inválido</p>`
         document.getElementById("email").style.border = "solid 1px red"
+        document.getElementById("iconerroemail").style.display = "block"
     }
     if (senha == "" || senha.length < 5){
         document.getElementById('errosenha').innerHTML = `<p>ERRO! Senha inválida</p>`
         document.getElementById("senha").style.border = "solid 1px red"
+        document.getElementById("iconerrosenha").style.display = "block"
     }
     if (termouso.checked == false){
         document.getElementById("errotermouso").innerHTML = `<p>Aceite os termos de uso para continuar</p>`
@@ -73,6 +87,12 @@ function validarempresa() {
     if (confirmarsenha != senha){
         document.getElementById('erroconfirmarsenha').innerHTML = `<p>ERRO! Senhas diferentes</p>`
         document.getElementById("confirmarsennha").style.border = "solid 1px red"
+        document.getElementById("iconerroconfsenha").style.display = "block"
+    }
+    if(nome.length > 3 && email != "" &&  cnpj == 14 && senha.length > 5 &&
+    confirmarsenha == senha && termouso.checked == true) {
+        alert("CADASTRO REALIZADO COM SUCESSO")
+        window.location.href = "../paginas/login.html";
     }
 }
 function validarprofessor() {
@@ -83,27 +103,32 @@ function validarprofessor() {
     var senha = document.getElementById("senha").value.trim();
     var confirmarsenha = document.getElementById("confirmarsenha").value.trim();
     var termouso = document.getElementById("termouso");
-    document.getElementById("cadastro-professor").style.height = "1050px"
+    document.getElementById("cadastro-professor").style.height = "1150px"
 
     if(nome == "" || nome.length < 3){
         document.getElementById("erronome").innerHTML = `<p>ERRO! Nome inválido</p>`
         document.getElementById("nome").style.border = "solid 1px red"
+        document.getElementById("iconerronome").style.display = "block"
     }
     if (sobrenome == ""){
         document.getElementById("errosobrenome").innerHTML = `<p>ERRO! Sobrenome inválido</p>`
         document.getElementById("sobrenome").style.border = "solid 1px red"
+        document.getElementById("iconerrosobren").style.display = "block"
     }
     if (email == ""){
         document.getElementById("erroemail").innerHTML = `<p>ERRO! E-mail inválido</p>`
         document.getElementById("email").style.border = "solid 1px red"
+        document.getElementById("iconerroemail").style.display = "block"
     }
     if (curso == "") {
         document.getElementById("errocursograduacao").innerHTML = `<p>ERRO! Curso inválido</p>`
         document.getElementById("cursograduacao").style.border = "solid 1px red"
+        document.getElementById("iconerrograduacao").style.display = "block"
     }
     if (senha == "" || senha.length < 5){
         document.getElementById("errosenha").innerHTML = `<p>ERRO! Senha inválida</p>`
         document.getElementById("senha").style.border = "solid 1px red"
+        document.getElementById("iconerrosenha").style.display = "block"
     }
     if (termouso.checked == false){
         document.getElementById("errotermouso").innerHTML =`<p>Aceite os termos de uso para continuar</p>`
@@ -111,38 +136,57 @@ function validarprofessor() {
     if (confirmarsenha != senha){
         document.getElementById("erroconfirmarsenha").innerHTML = `<p>ERRO! Senhas diferentes</p>`
         document.getElementById("confirmarsenha").style.border = "solid 1px red"
+        document.getElementById("iconerroconfsenha").style.display = "block"
+    }
+    if (nome.length > 3 && sobrenome != "" && email != "" && curso != "" && senha.length > 5 &&
+        confirmarsenha == senha && termouso.checked == true){
+        alert("CADASTRO REALIZADO COM SUCESSO")
+        window.location.href = "../paginas/login.html";
     }
 }
 function validarlogin() {
     var email = document.getElementById("email").value.trim();
     var senha = document.getElementById("senha").value.trim();
-    
+    document.getElementById("tela-login").style.height = "630px"
+
     if (email == ""){
         document.getElementById('erroemail').innerHTML = `<p>ERRO! E-mail inválido</p>`
         document.getElementById("email").style.border = "solid 1px red"
+        document.getElementById("iconerroemail").style.display = "block"
     }
     if (senha == ""){
-        document.getElementById('errosenha').innerHTML = `<p>ERRO! Senha inválida</p>`
+        document.getElementById("errosenha").innerHTML = `<p>ERRO! Senha inválida</p>`
         document.getElementById("senha").style.border = "solid 1px red"
+        document.getElementById("iconerrosenha").style.display = "block"
     }
-    document.getElementById("tela-login").style.height = "580px"
+    if (email != "" && senha != ""){
+        window.location.href = "#";
+    }
+
 }
 function validarsenhanova(){
     var email = document.getElementById("email").value.trim();
     var senha = document.getElementById("senha").value.trim();
     var confirmarsenha = document.getElementById("confirmarsenha").value.trim();
-    document.getElementById("tela-recuperarsenha").style.height = "625px"
+    document.getElementById("tela-recuperarsenha").style.height = "680px"
 
     if (email == ""){
         document.getElementById("erroemail").innerHTML = `<p>ERRO! E-mail inválido</p>`
         document.getElementById("email").style.border = "solid 1px red"
+        document.getElementById("iconerroemail").style.display = "block"
     }
     if (senha == "" || senha.length < 5){
         document.getElementById("errosenha").innerHTML = `<p>ERRO! Senha inválida</p>`
         document.getElementById("senha").style.border = "solid 1px red"
+        document.getElementById("iconerrosenha").style.display = "block"
     }
     if (confirmarsenha != senha){
         document.getElementById("erroconfirmarsenha").innerHTML = `<p>ERRO! Senhas diferentes</p>`
         document.getElementById("confirmarsenha").style.border = "solid 1px red"
+        document.getElementById("iconerroconfsenha").style.display = "block"
+    }
+    if (email != "" && senha.length > 5 && confirmarsenha == senha ){
+        alert("SENHA ALTERADA COM SUCESSO")
+        window.location.href = "../paginas/login.html";
     }
 }
